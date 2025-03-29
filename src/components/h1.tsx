929678938e6cc3c14da1bcd610a28b20b5f6ae04
denexapp <mail@denexapp.me>
2025-03-29 20:34:25 +0800

    Fix H1 color on light theme

--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -2,13 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 const H1: FC<PropsWithChildren> = (props) => {
   const { children } = props;
-  return (
-    <h1
-      className="text-2xl text-primary-text"
-    >
-      {children}
-    </h1>
-  );
+  return <h1 className="text-2xl text-text">{children}</h1>;
 };
 
 export default H1;
