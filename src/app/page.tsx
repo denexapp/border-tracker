fcb4ee69045607be89b9dd316d8f31b13d63c380
denexapp <mail@denexapp.me>
2025-03-31 18:35:38 +0800

    Add padding for pages, and fix layout of the track page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import H1 from "@/components/h1";
 
 export default async function Page() {
   return (
-    <div className="flex flex-col gap-6 items-center justify-center">
+    <div className="flex flex-col gap-6 items-center justify-center p-2">
       <H1>border tracker</H1>
       <ButtonLink href="/track">track</ButtonLink>
     </div>
