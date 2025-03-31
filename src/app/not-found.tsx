4338e087f51fb3d6ec2f8d2ea05683d4da735f1a
denexapp <mail@denexapp.me>
2025-03-31 18:29:31 +0800

    Add a 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Text from "@/components/text";
+
+export default function Page() {
+  return (
+    <div className="grid place-items-center p-2">
+      <Text>page is not found</Text>
+    </div>
+  );
+}
