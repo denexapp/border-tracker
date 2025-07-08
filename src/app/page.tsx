67d1b8d6805a90eb73d5f2f2a6d3932d8e9d7a8c
denexapp <mail@denexapp.me>
2025-07-08 09:48:57 +0800

    refactor: improve entry rendering logic and layout in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,29 @@ import { getEntries } from "@/entities/entry/api/getEntries";
 import EntryComponentLink from "@/entities/entry/ui/entryComponentLink";
 import ButtonForm from "@/shared/ui/button/buttonForm";
 import H1 from "@/shared/ui/text/h1";
-import { FC } from "react";
+import Text from "@/shared/ui/text/text";
+import { FC, ReactNode } from "react";
 
 const Page: FC = async () => {
   await validateSession();
   const entries = await getEntries();
+  let content: ReactNode;
 
-  const entryNodes = entries.map((entry) => (
-    <EntryComponentLink key={entry.id} entry={entry} href={`/entries/${entry.id}`} />
-  ));
+  if (entries.length === 0) {
+    content = <Text>no entries yet</Text>;
+  } else {
+    content = entries.map((entry) => <EntryComponentLink key={entry.id} entry={entry} href={`/entries/${entry.id}`} />);
+  }
 
   return (
     <div className="flex flex-col gap-2 items-center justify-center p-2">
       <H1 className="py-2">entries</H1>
-      <div className="flex flex-col gap-2 max-w-xl w-full">{entryNodes}</div>
-      <ButtonForm className="sticky bottom-2" onClick={addEntryAndRedirect}>
-        add new entry
-      </ButtonForm>
+      <div className="flex gap-2 items-center w-full flex-col-reverse">
+        <ButtonForm className="sticky bottom-2" onClick={addEntryAndRedirect}>
+          add new entry
+        </ButtonForm>
+        <div className="flex flex-col gap-2 max-w-xl w-full">{content}</div>
+      </div>
     </div>
   );
 };
